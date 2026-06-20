7718dfc862190ddd6546eb2cef36d158a3675c37
christianyoga13 <yogashandy47@gmail.com>
2026-06-20 19:35:49 +0700

    fix redirect link1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import LivingCollections from "./pages/LivingCollection";
 import ProductAR from "./pages/ProductAR";
@@ -10,7 +10,7 @@ import OnboardingTour from "./components/OnboardingTour";
 function App() {
   return (
     <WishlistProvider>
-      <HashRouter>
+      <BrowserRouter>
         <OnboardingTour />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,7 +19,7 @@ function App() {
           <Route path="/products/:slug" element={<ProductDetail />} />
           <Route path="/wishlist" element={<Wishlist />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </WishlistProvider>
   );
 }
